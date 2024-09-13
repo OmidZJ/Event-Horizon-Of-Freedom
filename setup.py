3362c5393c962da012aa1b831882702f66de7fc9
OmidZJ <zamanijamshidi.omid@gmail.com>
2024-09-13 22:29:44 +0330

    some changes

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='eventhorizon',
     version='0.1',
-    description='A tool for testing and configuring VLESS links with Xray.',
-    author='Your Name',
-    author_email='your.email@example.com',
     packages=find_packages(),
     install_requires=[
         'httpx',
@@ -13,7 +10,14 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'eventhorizon = eventhorizon.__main__:main',
+            'eventhorizon = eventhorizon.core:main',
         ],
     },
+    description='A tool for scanning domains for Fastly, G Core, Cloudflare, and other services.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/OmidZJ/Event-Horizon-Of-Freedom',  # Update this with your actual repository URL
+    author='Omid',
+    author_email='zamanijamshidi.omid@gmail.com',
+    license='MIT',
 )
